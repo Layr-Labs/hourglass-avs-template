4963fd3b3d7299699c5ba62a79a3559cb2f97b74
0xrajath <rajathalex@gmail.com>
2025-06-30 12:58:26 -0400

    fix: forge build

--- contracts/src/l2-contracts/AVSTaskHook.sol
+++ contracts/src/l2-contracts/AVSTaskHook.sol
@@ -24,7 +24,7 @@ contract AVSTaskHook is IAVSTaskHook {
 
     function handleTaskResultSubmission(
         bytes32, /*taskHash*/
-        IBN254CertificateVerifierTypes.BN254Certificate memory /*cert*/
+        bytes memory /*cert*/
     ) external {
         //TODO: Implement
     }
