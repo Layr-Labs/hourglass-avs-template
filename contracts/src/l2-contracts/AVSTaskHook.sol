f142fbabb24969387545544a73c93051b5fde5e9
Grezle <4310551+grezle@users.noreply.github.com>
2025-07-09 01:15:09 +0100

    fix: add templates for new AVSTaskHook methods

--- contracts/src/l2-contracts/AVSTaskHook.sol
+++ contracts/src/l2-contracts/AVSTaskHook.sol
@@ -7,25 +7,42 @@ import {OperatorSet} from "@eigenlayer-contracts/src/contracts/libraries/Operato
 
 import {IAVSTaskHook} from "@hourglass-monorepo/src/interfaces/avs/l2/IAVSTaskHook.sol";
 
+import {ITaskMailboxTypes} from "@hourglass-monorepo/src/interfaces/core/ITaskMailbox.sol";
+
 contract AVSTaskHook is IAVSTaskHook {
     function validatePreTaskCreation(
-        address, /*caller*/
-        OperatorSet memory, /*operatorSet*/
-        bytes memory /*payload*/
-    ) external view {
-        //TODO: Implement
+        address caller,
+        ITaskMailboxTypes.TaskParams memory taskParams
+    ) external view override {
+        // TODO: Implement
     }
 
     function handlePostTaskCreation(
-        bytes32 /*taskHash*/
-    ) external {
-        //TODO: Implement
+        bytes32 taskHash
+    ) external override {
+        // TODO: Implement
     }
 
-    function handleTaskResultSubmission(
-        bytes32, /*taskHash*/
-        bytes memory /*cert*/
-    ) external {
-        //TODO: Implement
+    function validatePreTaskResultSubmission(
+        address caller,
+        bytes32 taskHash,
+        bytes memory cert,
+        bytes memory result
+    ) external view override {
+        // TODO: Implement
+    }
+
+    function handlePostTaskResultSubmission(
+        bytes32 taskHash
+    ) external override {
+        // TODO: Implement
+    }
+
+    function calculateTaskFee(
+        OperatorSet memory operatorSet,
+        bytes memory payload
+    ) external view override returns (uint96) {
+        // TODO: Implement
+        return 0;
     }
 }
