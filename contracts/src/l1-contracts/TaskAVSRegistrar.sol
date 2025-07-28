c5ed90292993dda3e0ca4e5b3eb9b7d231cfae58
0xrajath <rajathalex@gmail.com>
2025-07-28 11:59:00 -0400

    chore: refactored .devkit/contracts

--- contracts/src/l1-contracts/TaskAVSRegistrar.sol
+++ contracts/src/l1-contracts/TaskAVSRegistrar.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.27;
 
 import {IAllocationManager} from "@eigenlayer-contracts/src/contracts/interfaces/IAllocationManager.sol";
 import {IKeyRegistrar} from "@eigenlayer-contracts/src/contracts/interfaces/IKeyRegistrar.sol";
-
-import {TaskAVSRegistrarBase} from "@hourglass-monorepo/src/avs/TaskAVSRegistrarBase.sol";
+import {TaskAVSRegistrarBase} from "@eigenlayer-middleware/src/avs/task/TaskAVSRegistrarBase.sol";
 
 contract TaskAVSRegistrar is TaskAVSRegistrarBase {
     /**
